09051a97b4a7706c8952255a6fab22b156e0e2e8
Kartik Verma <kartikve@amdocs.com>
2026-02-23 12:33:05 +0530

    Added functionality to process_api_data

--- java/local/src/main/java/com/amdocs/sanity/SanityRunner.java
+++ java/local/src/main/java/com/amdocs/sanity/SanityRunner.java
@@ -14,17 +14,18 @@ public final class SanityRunner {
         int argsForExtendedSanity = 11;
         if (args.length != argsForBasicSanity && args.length != argsForExtendedSanity) {
             System.err.println(
-                    "Usage: java SanityRunner <input_junit_report_dir> <output_dir> <job_name> <input_api_responses_dir> <output_failed_responses_dir>");
+                    "Usage: java SanityRunner <input_junit_report_dir> <input_tc_data_dir> <output_dir> <job_name>");
             System.err.println(
-                    "Usage: java SanityRunner <input_junit_report_dir> <output_dir> <job_name> <input_api_responses_dir> <output_failed_responses_dir> <input_log_file> <flows_separated_by_|> <project (OE/CO)> <DMP> <env> <tester_name>");
+                    "Usage: java SanityRunner <input_junit_report_dir> <input_tc_data_dir> <output_dir> <job_name> <input_log_file> <flows_separated_by_|> <project (OE/CO)> <DMP> <env> <tester_name>");
             System.exit(1);
         }
 
         String junitReportDir = args[0];
-        String outputDir = args[1];
-        String jobName = args[2];
-        String apiRespDir = args[3];
-        Path failedApiRespDir = Paths.get(args[4]);
+        String testCaseDataDir = args[1];
+        String outputDir = args[2];
+        String jobName = args[3];
+        Path processedTestCaseDataDir = Paths.get(outputDir).resolve("processed_tc_data");
+        Path failedTestCaseDataDir = Paths.get(outputDir).resolve("failed_tc_data");
 
         int exitCode = 0;
 
@@ -33,7 +34,7 @@ public final class SanityRunner {
                     junitReportDir,
                     outputDir,
                     jobName,
-                    apiRespDir,
+                    testCaseDataDir,
                     System.out::println);
 
             System.out.println("\nSummary:");
@@ -42,32 +43,34 @@ public final class SanityRunner {
             System.out.println("  Failed: " + summary.totalFailed);
             System.out.println("  Total Time: " + String.format("%.3f", summary.totalTime) + "s");
 
-            CopyFailedResponses.copy(Paths.get(apiRespDir), failedApiRespDir);
-            System.out.println("Copied Failed Responses!");
+            ProcessAPIResults.processResultFiles(Paths.get(testCaseDataDir), processedTestCaseDataDir);
+
+            CopyFailedResponses.copy(processedTestCaseDataDir, failedTestCaseDataDir);
+            System.out.println("Copied Failed Test Case Data!");
         } catch (Exception e) {
             exitCode = 1;
             e.printStackTrace();
         }
 
         if (args.length == argsForExtendedSanity) {
-            Path originalPath = Paths.get(args[5]);
+            Path originalPath = Paths.get(args[4]);
             String originalFileName = originalPath.getFileName().toString();
             Path excelPath = Paths.get(outputDir, "Exceptions.xlsx");
-            String[] flows = args[6].split("\\|");
+            String[] flows = args[5].split("\\|");
             
             int project = -1;
-            if (args[7].equalsIgnoreCase("OE")) {
+            if (args[6].equalsIgnoreCase("OE")) {
                 project = 1;
-            } else if (args[7].equalsIgnoreCase("CO")) {
+            } else if (args[6].equalsIgnoreCase("CO")) {
                 project = 2;
             } else {
                 System.err.println("Project must be OE or CO");
                 System.exit(1);
             }
 
-            String dmp = args[8];
-            String env = args[9];
-            String tester = args[10];
+            String dmp = args[7];
+            String env = args[8];
+            String tester = args[9];
 
             try {
                 for (String flow : flows) {
